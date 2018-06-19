3920a870292090a2b12677886197215647d4c692
Olivier Mengué <dolmen@cpan.org>
2018-06-19 23:44:53 +0200

    Add testable example

---
+++ example_amd64_test.go
@@ -0,0 +1,20 @@
+package endian_test
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/dolmen-go/endian"
+)
+
+func Example() {
+	fmt.Printf("GOARCH=%s: %s\n", runtime.GOARCH, endian.Native)
+
+	const n = 0xDeadBeef
+	var b [4]byte
+	endian.Native.PutUint32(b[:], n)
+	fmt.Printf("0x%x => [% x]\n", n, b)
+	// Output:
+	// GOARCH=amd64: LittleEndian
+	// 0xdeadbeef => [ef be ad de]
+}
