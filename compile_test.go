e757534e4b736a9e4b2b9310dfaeb73d2ee9e1da
Olivier Mengué <dolmen@cpan.org>
2024-08-26 22:45:56 +0200

    Add build tag endian_nostatic
    Add build tag endian_nostatic to force the use of the dynamic
    implementation at compile time with "go build -tags endian_nostatic".
    
    To run the testsuite with both implementations:
      go test -run TestDynamic -v -short
      go test -run TestDynamic -v -short -tags endian_nostatic

--- compile_test.go
+++ compile_test.go
@@ -5,6 +5,7 @@ package endian_test
 
 import (
 	"encoding/binary"
+	"reflect"
 	"testing"
 
 	"github.com/dolmen-go/endian"
@@ -32,4 +33,22 @@ func TestEqual(t *testing.T) {
 	if nbOK != 1 {
 		t.Error("1 equal expected, got", nbOK)
 	}
+}
+
+// go test -run TestDynamic -v -short
+// go test -run TestDynamic -v -short -tags endian_nostatic
+func TestDynamic(t *testing.T) {
+	typ := reflect.TypeOf(&endian.Native).Elem()
+
+	if typ.Kind() == reflect.Interface {
+		// To test this case: go test -tags endian_nostatic -v -run TestDynamic
+		t.Log("endian.Native is dynamic")
+	} else {
+		t.Log("endian.Native is static")
+	}
+
+	if testing.Short() {
+		return
+	}
+	t.Log(typ)
 }
