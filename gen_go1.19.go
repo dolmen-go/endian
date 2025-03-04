b74493d2d34fb2afcbd908dfe0850731ec7f7467
Olivier Mengué <dolmen@cpan.org>
2025-03-04 15:34:46 +0100

    Rename generate_go1.19.go to generate.go

--- gen_go1.19.go
+++ gen_go1.19.go
@@ -1,6 +1,6 @@
 //go:build !endiangen && go1.19
 // +build !endiangen,go1.19
 
-//go:generate go run -tags endiangen generate_go1.19.go
+//go:generate go run -tags endiangen generate.go
 
 package endian
